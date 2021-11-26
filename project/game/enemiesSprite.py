1259389f17c9e4e2e682bf69ee3bba4b590c4314
Santiago Pintus <santiagopintus@gmail.com>
2021-11-26 18:23:33 -0300

    Enemy spawn is working great!

--- project/game/enemiesSprite.py
+++ project/game/enemiesSprite.py
@@ -18,11 +18,22 @@ class EnemiesSprite(arcade.Sprite):
 
         self.guid = "Enemy"
 
-        self.center_y = random.randrange(self._constants.BOTTOM_LIMIT, self._constants.TOP_LIMIT)
-        self.center_x = random.randrange(self._constants.LEFT_LIMIT, self._constants.RIGHT_LIMIT)
+        # Here we make enemies spawn outside the screen
+        self.center_y = random.choice(
+            [
+            random.randrange(self._constants.BOTTOM_LIMIT, 0), # Bottom limit to 0
+            random.randrange(0, self._constants.TOP_LIMIT) # 0 to top limit
+            ]
+            )
+        self.center_x = random.choice(
+            [
+            random.randrange(self._constants.LEFT_LIMIT, 0), # Left limit to 0
+            random.randrange(self._constants.SCREEN_WIDTH, self._constants.RIGHT_LIMIT) # 0 to right limit
+            ]
+        )
 
-        self.change_x = random.random() * 3
-        self.change_y = random.random() * 3
+        self.change_x = random.choice([random.random() * 3, random.random() * -3])
+        self.change_y = random.choice([random.random() * 3, random.random() * -3])
 
         self.angle = math.degrees(math.atan2(self.change_y, self.change_x)) - 90
 
